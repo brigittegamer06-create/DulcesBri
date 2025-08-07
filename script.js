c961c6f4b12b7b1a60506341e17167d589d47889
Brigitte <brigittegamer06@gmail.com>
2025-08-07 11:10:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-let total = 0;
-
-function agregarAlCarrito(nombre, precio) {
-  total += precio;
-  actualizarCarrito();
-}
-
-function actualizarCarrito() {
-  const totalTexto = document.getElementById('total');
-  totalTexto.textContent = `Total: Bs. ${total.toFixed(2)}`;
-}
 
